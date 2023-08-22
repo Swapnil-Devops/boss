29f306ee4aca0071d88219b5a17f14b24dcbb572
swapnil <swapnilgawali244@gmail.com>
2023-08-22 16:30:07 +0530

    set action files

---
+++ index.js
@@ -0,0 +1,23 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try {
+  const commentBody = github.context.payload.comment.body;
+  const discussionId = github.context.payload.discussion.id;
+  const discussionNodeId = github.context.payload.discussion.node_id;
+  const discussionBody = github.context.payload.discussion.body;
+
+  console.log(`Comment Body: ${commentBody}`);
+  console.log(`Discussion ID: ${discussionId}`);
+  console.log(`Discussion Node ID: ${discussionNodeId}`);
+  console.log(`Discussion Body: ${discussionBody}`);
+
+  // You can add your custom logic here to perform actions using the comment and discussion data.
+
+  // Set an output for the comment body, if needed.
+  core.setOutput("comment_body", commentBody);
+  core.setOutput("disc_ID", discussionNodeId);
+  core.setOutput("disc_body", discussionBody);
+} catch (error) {
+  core.setFailed(error.message);
+}
